f69954e103fa672315bf7120f49fabf0944ac936
mjc4070 <92746438+mjc4070@users.noreply.github.com>
2025-11-04 12:10:22 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'page_home.dart';
+import 'page_search.dart';
+import 'page_settings.dart';
+import 'page_profile.dart';
+
+int currentBottomTab = 0;
+
+final List<Widget> bottomNavScreens = [
+  PageHome(),
+  PageSearch(),
+  PageSettings(),
+  PageProfile(),
+];
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,14 +23,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Demo11a(),
-    );
+    return const MaterialApp(home: Demo11a());
   }
 }
 
 class Demo11a extends StatefulWidget {
   const Demo11a({super.key});
+
+  @override
+  State<Demo11a> createState() => _Demo11aState();
+}
+
+class _Demo11aState extends State<Demo11a> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Week 11B Demo")),
+      body: bottomNavScreens[currentBottomTab],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: currentBottomTab,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home",backgroundColor: Colors.purple),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search",backgroundColor:Colors.blue),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.portrait),
+            label: "Profile",
+            backgroundColor: Colors.green,
+          ),
+        ],
+        onTap: (value) {
+          setState(() {
+            currentBottomTab = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
 
   @override
   State<Demo11a> createState() => _Demo11aState();
